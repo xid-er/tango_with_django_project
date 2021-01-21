3ea7d3f23e3cfd795791727993f482247d053aa7
Karlis Siders <2467273s@student.gla.ac.uk>
2021-01-21 17:33:20 +0000

    most viewed pages

--- rango/views.py
+++ rango/views.py
@@ -7,10 +7,12 @@ def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
